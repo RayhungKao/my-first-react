42d4cc60b300c300dd31af1f15b6d3d791fe4bde
Rayhung Kao <ryamkao2339@gmail.com>
2020-06-02 20:04:23 +0800

    Updates

---
+++ precache-manifest.d4376be2e1c81b8ff2fd4ed6f103bbb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7386476707fede3810195d01c012a0",
+    "url": "/my-first-react/index.html"
+  },
+  {
+    "revision": "53a0d43df608ccb584e0",
+    "url": "/my-first-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65969750947a715475e0",
+    "url": "/my-first-react/static/js/2.2e06293d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react/static/js/2.2e06293d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a0d43df608ccb584e0",
+    "url": "/my-first-react/static/js/main.bf831ea8.chunk.js"
+  },
+  {
+    "revision": "dd98bdcc63c4433c4176",
+    "url": "/my-first-react/static/js/runtime-main.52a4880b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
